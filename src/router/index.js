4fa1223c3a21ed68cfc0123bb218c2f37b7d2f48
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2025-05-12 11:31:34 -0300

    Autenticação de login pra cada rota

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CadastroFornecedor from '../views/CadastroFornecedor.vue'
 import CadastroCliente from '../views/CadastroCliente.vue'
+import OfertaEnergia from '../views/OfertaEnergia.vue'
+import Dashboard from '../views/Dashboard.vue'
+
 
 // Criar a lista de páginas
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/fornecedor', name: 'CadastroFornecedor', component: CadastroFornecedor },
   { path: '/cliente', name: 'CadastroCliente', component: CadastroCliente },
+  { path: '/OfertaEnergia', name: 'OfertaEnergia', component: OfertaEnergia },
+  { path: '/Dashboard', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem('usuarioLogado');
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/');
+  } else {
+    next();
+  }
+});
 
 export default router
